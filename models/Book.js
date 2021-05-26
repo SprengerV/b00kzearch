094f6bade88ab6c703e691f04d57e883aa4d9cd6
SprengerV <B.Francis@SprengerV.com>
2021-05-26 17:37:11 -0400

    book schema

---
+++ models/Book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: [String],
+    description: String,
+    image: String,
+    link: String
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
