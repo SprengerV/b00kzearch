c2c807d759b913ed6efe916a2790607a956524ca
SprengerV <B.Francis@SprengerV.com>
2021-05-27 00:28:46 -0400

    dynamic header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/nav';
+import Header from './components/header';
 import Search from './views/search';
 import Saved from './views/saved';
 import './App.css';
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Nav/>
+        <Header/>
         <Route exact path={ ["/", "/search"] } component={ Search }/>
         <Route exact path="/saved" component={ Saved }/>
       </Router>
---
+++ client/src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import './header.css';
+
+const Header = (props) => {
+  return (
+    <Row>
+      <Col xs="11" className="ms-auto me-auto">
+        <Card id="headerBox" className="w-100">
+          <Container id="header" className="d-flex flex-column justify-content-center">
+            { props.location.pathname === '/search' && <h2>Search your favorite titles</h2> }
+            { props.location.pathname === '/saved' && <h2>Your saved books</h2> }
+            <p>Powered by Google Books API</p>
+          </Container>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default withRouter(Header);
