e1738e47b36e200e9d5473fc70f7a9a537570295
SprengerV <B.Francis@SprengerV.com>
2021-05-26 23:41:23 -0400

    working navbar

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav as Nv, Container, Row } from 'react-bootstrap';
+import './nav.css';
 
 const Nav = () => {
     return (
         <Navbar bg="info" expand="md">
-            <Link to="/">
-                <Navbar.Brand>b00kzearch</Navbar.Brand>
-            </Link>
+            <Container fluid={ true }>
+                    <Link to="/">
+                        <Navbar.Brand id="brand">b00kzearch</Navbar.Brand>
+                    </Link>
+                    <Navbar.Toggle aria-controls="nav-collapse"/>
+                    <Navbar.Collapse id="nav-collapse" className="flex-row-reverse">
+                        <Nv.Link>
+                            <Link className="text-dark" to="/search">Search</Link>
+                        </Nv.Link>
+                        <Nv.Link>
+                            <Link className="text-dark" to="/saved">Saved</Link>
+                        </Nv.Link>             
+                    </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 };
