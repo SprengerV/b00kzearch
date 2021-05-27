1c15e44c7a229f32aaf1f981c618d3a09d8cd117
SprengerV <B.Francis@SprengerV.com>
2021-05-27 05:10:24 -0400

    conditional render of book in search

---
+++ client/src/components/searchForm/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import './searchForm.css';
+
+const SearchForm = (props) => {
+  return (<>
+    <Row>
+      <Col xs="8" className="ms-auto me-auto">
+        <Card id="searchBox" className="w-100">
+          <Container>
+            <Col xs="12" md="8" lg="6" xl="5" className="ms-auto me-auto">
+              <Form id="searchForm" className="d-flex w-100" onSubmit={props.search}>
+                <Form.Control type="text" placeholder="Enter search terms here" />
+                <Button id="searchBtn" variant="outline-info" type="submit">
+                  &#9885;
+                </Button>
+              </Form>
+            </Col>
+          </Container>
+        </Card>
+      </Col>
+    </Row>
+  </>);
+};
+
+export default SearchForm;
--- client/src/views/search.js
+++ client/src/views/search.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import SearchForm from '../components/searchForm';
+import NoBooks from '../components/noBooks';
+import Book from '../component/book';
 
-const Search = (props) => {
+const Search = () => {
+  const [result, setResult] = useState(null)
+
   return (<>
-    <Row>
-      <Col xs="8" className="ms-auto me-auto">
-        <Card id="searchBox" className="w-100">
-          <Container>
-            <Col xs="12" md="8" lg="6" xl="5" className="ms-auto me-auto">
-              <Form id="searchForm" className="d-flex w-100" onSubmit={ props.search }>
-                <Form.Control type="text" placeholder="Enter search terms here"/>
-                <Button id="searchBtn" variant="outline-info" type="submit">
-                  &#9885;
-                </Button>  
-              </Form>
-            </Col>
-          </Container>
-        </Card>
-      </Col>
-    </Row>
+    <SearchForm/>
+    { result 
+      ? <NoBooks/>
+      : <Book data={ result }/> }
   </>);
 };
 
